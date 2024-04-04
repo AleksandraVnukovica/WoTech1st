9ddacc6500230a05183c4efa298af0ec86c99066
Aleksandra <165931666+AleksandraVnukovica@users.noreply.github.com>
2024-04-04 18:09:37 +0300

    Add files via upload

--- main.java
+++ main.java
@@ -1,31 +1,42 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
 public class Main {
   public static void main(String[] args) {
-
+    // System.out.println("Hello world!");
     // winter, spring, summer, autumn
-    // warm jacket, T-shirt, swimming suite, rain coat
+    // warm jacket, t-shirt, swimming suite, rain coat
+    String season = "qwerty";
 
-    double temp = -15.0;
-
-    if (temp < 5) {
-      System.out.println("Wear super warm clothes!");
-    }
-    else if (temperature >= 5 && temperature < 15) {
-      System.out.println("Wear warm clothes!");
-    }
-    else if (temperature >= 15 && temperature < 30) {
-      System.out.println("Wear a normal clothes!");
-    }
-    else if (temperature >= 30) {
-      System.out.println("Wear a cooling clothes!");
-    }
-    else {
-      System.out.println("I do not recognize this season");
-    }
+    if (season == "winter") {
+      System.out.println("Wear a warm jacket!");
+    } else if (season == "spring") {
+      System.out.println("Wear a T-shirt!");
+    } else if (season == "summer") {
+      System.out.println("Wear a swimming suite!");
+    } else if (season == "autumn") {
+      System.out.println("Wear a rain coat!");
+    } else {
+      System.out.println("I do not recongnize this season!");
     }
 
+    double temp = -10.23;
 
+    if (temp <= 5) {
+      System.out.println("Wear super warm");
+    } else if (temp <= 15) {
+      System.out.println("Wear warm");
+    } else if (temp <= 30) {
+      System.out.println("Wear normal");
+    } else {
+      System.out.println("You need cooling");
+    }
+
+  }
+
+  // @Test
+  // void addition() {
+  // assertEquals(2, 1 + 1);
+  // }
 }
-// till +5 wear super warm
-// +5 till 15 warm
-//+15 to 30 normal
-//+30+ cooling
